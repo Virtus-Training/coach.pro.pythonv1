4cde0c2320983b01ac2925a8e46788ca3d247f37
Virtus-Training <eric9925@gmail.com>
2025-08-15 11:55:05 +0200

    feat: add exercise tags filtering

--- models/exercices.py
+++ models/exercices.py
@@ -8,6 +8,7 @@ class Exercise:
     nom: str
     groupe_musculaire_principal: str
     equipement: Optional[str] = None
+    tags: Optional[str] = None
     type_effort: str = ""
     coefficient_volume: float = 1.0
     est_chargeable: bool = False
