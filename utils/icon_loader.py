1e33c78530f1b93631aef0e42da06f03d2ae523a
Virtus-Training <eric9925@gmail.com>
2025-08-21 14:58:03 +0200

    refactor(app): integrate AppShell as main window layout

--- utils/icon_loader.py
+++ utils/icon_loader.py
@@ -1,1 +1,31 @@
+import os
+from typing import Tuple
 
+import customtkinter as ctk
+from PIL import Image
+
+# Simple in-memory cache for loaded icons
+_ICON_CACHE: dict[Tuple[str, Tuple[int, int]], ctk.CTkImage] = {}
+
+
+def load_icon(filename: str, size: Tuple[int, int] | int) -> ctk.CTkImage:
+    """Load an icon from assets/icons with basic caching.
+
+    Parameters
+    ----------
+    filename: str
+        Name of the icon file located in ``assets/icons``.
+    size: tuple[int, int] | int
+        Desired size of the icon. If an int is provided, a square icon is
+        created.
+    """
+
+    if isinstance(size, int):
+        size = (size, size)
+
+    key = (filename, size)
+    if key not in _ICON_CACHE:
+        path = os.path.join("assets", "icons", filename)
+        image = Image.open(path)
+        _ICON_CACHE[key] = ctk.CTkImage(image, size=size)
+    return _ICON_CACHE[key]
